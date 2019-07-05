4467d5156bcc62f88b0a1204dea0985b0d78f64f
WWZ <zeing@outlook.com>
2019-07-06 02:32:15 +0700

    feat: get query location from req

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,18 @@ const router = Router();
 
 /* GET index page. */
 router.get('/', (req, res) => {
+  let location = req.query.location;
+  console.log(location)
   axios.get('https://maps.googleapis.com/maps/api/place/textsearch/json?', {
     params: {
       key: 'AIzaSyDl_k1hquEPQpUcQBoMi4CvIt0uqeLH2cs',
-      location: 'bang sue',
-      query: 'bang%sue',
+      query: location,
       type : 'restaurant',
       region: 'th'
     }
   })
     .then(data => {
-      console.log(data.data.results);
+      // console.log(data.data.results);
       res.json(data.data.results);
 
     });
