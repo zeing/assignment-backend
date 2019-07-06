6c21dd7366c182a5831e720e6966abb944ea387c
WWZ <zeing@outlook.com>
2019-07-06 17:12:35 +0700

    feat: add route to find series sequence

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,60 @@
 const { Router } = require('express');
-const axios = require('axios');
 const router = Router();
+const redis = require('../redis');
 
-/* GET index page. */
-router.get('/', (req, res) => {
-  let location = req.query.location;
-  console.log(location)
-  axios.get('https://maps.googleapis.com/maps/api/place/textsearch/json?', {
-    params: {
-      key: process.env.GOOGLE_API_KEY,
-      query: location,
-      type : 'restaurant',
-      region: 'th'
-    }
-  })
-    .then(data => {
-      // console.log(data.data.results);
-      res.json(data.data.results);
-
+const seriesSequenceKey = process.env.REDIS_SERIES_SEQUENCE_KEY;
+const setSeriesSequenceCache =  (numberBox) => {
+    return redis.set(seriesSequenceKey, JSON.stringify(numberBox), (err) => {
+        return err
     });
+}
 
+function getSeriesSequenceCache() {
+    let numberBox = { 1: 3, 2: 5, 3: 9, 4: 15 };
+    const promise = new Promise((resolve, reject) => {
+        redis.get(seriesSequenceKey, (err, seriesSequence) => {
+            if (!seriesSequence) {
+                console.log('ไม่มี');
+                setSeriesSequenceCache(numberBox);
+                resolve(numberBox)
+            } else {
+                console.log('มี', JSON.parse(seriesSequence));
+                resolve(JSON.parse(seriesSequence))
+            }
+        });
+    });
+    return promise
+}
+
+async function findSeriesSequence(n) {
+    let seriesSequence = await getSeriesSequenceCache();
+    if (seriesSequence[n]) {
+        return seriesSequence[n]
+    } else {
+        let next = await findSeriesSequence(n - 1) + 2 * (n - 1);
+        seriesSequence = await getSeriesSequenceCache();
+        if (next) {
+            seriesSequence[n] = next;
+            await setSeriesSequenceCache(seriesSequence);
+            return next;
+        }
+    }
+};
+
+router.get('/:n',async (req, res) => {
+    let seriesSequence = await getSeriesSequenceCache();
+    // console.log(seriesSequence, req.params.n ,seriesSequence[req.params.n] );
+    if (seriesSequence[req.params.n]) {
+        return res.json({ source: 'cache', result: seriesSequence[req.params.n]})
+    } else {
+        let data = await findSeriesSequence(req.params.n);
+        if(data) {
+            res.json({source: 'api', result: data})
+        } else {
+            console.log('error')
+        }
+
+    }
 });
 
 module.exports = router;
