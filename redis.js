463b19fe1eacc8aaaad5da492d32588e23d381d7
Witthawin Sirisiwaphak <eing.wi@gettgo.com>
2020-01-30 15:16:29 +0700

    update project

--- redis.js
+++ redis.js
@@ -1,13 +1,14 @@
 const redis = require('redis');
-const client = redis.createClient(process.env.REDIS_PORT,process.env.REDIS_HOST);
 
-client.on('ready',function() {
-    console.log("Redis is ready");
+const client = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
+
+client.on('ready', () => {
+  console.log('Redis is ready');
 });
 
 // echo redis errors to the console
 client.on('error', (err) => {
-    console.log("Error " + err)
+  console.log(`Error ${err}`);
 });
 
 // export const setCache = (key, data) => {
